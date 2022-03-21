57cc1ae38f4199b141cb3a666109736a97a0918e
Cris <mirocristy@gmail.com>
2022-03-21 16:03:44 +0100

    primer commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.write('- Generar 10 números aleatorios que estén comprendidos entre 0 y 20 haciendo uso de un bucle. ')
+document.write('<br>')
+document.write('<br>')
+
+for(i=0; i<10; i++){
+    document.write('<br>')
+    document.write(Math.floor(Math.random()* 20));
+
+};
+
